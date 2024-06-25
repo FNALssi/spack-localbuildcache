e5a1643219cd1c24278f07c8203553217d5af8c6
Marc Mengel <mengel@fnal.gov>
2024-06-25 15:44:45 -0500

    description, etc. in cmd file

--- localbuildcache/cmd/localbuildcache.py
+++ localbuildcache/cmd/localbuildcache.py
@@ -2,6 +2,9 @@ import sys
 import spack.config
 from spack.extensions import localbuildcache as lext
 
+description = "create buildcache of local packages in environment"
+section = "basic"
+level = "short"
 
 def setup_parser(subparser):
     subparser.add_argument(
