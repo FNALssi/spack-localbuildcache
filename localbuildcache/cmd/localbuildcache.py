7991f6b21a823c865d83c0bdeb593da4fbe1b561
Marc Mengel <mengel@fnal.gov>
2024-07-30 10:41:58 -0500

    black formatted

--- localbuildcache/cmd/localbuildcache.py
+++ localbuildcache/cmd/localbuildcache.py
@@ -6,25 +6,22 @@ description = "create buildcache of local packages in environment"
 section = "environments"
 level = "short"
 
+
 def setup_parser(subparser):
-    subparser.add_argument(
-        "--key",
-        default=None,
-        help="signing key for packges"
-    )
+    subparser.add_argument("--key", default=None, help="signing key for packges")
     subparser.add_argument(
         "--local",
         default=False,
         action="store_true",
-        help="Restrict to packages local to this instance (not upstream)"
+        help="Restrict to packages local to this instance (not upstream)",
     )
     subparser.add_argument(
         "--no-duplicates",
         default=False,
         action="store_true",
-        help="Restrict to packages not installed from a buildcache"
+        help="Restrict to packages not installed from a buildcache",
     )
 
-    
+
 def localbuildcache(parser, args):
     locext.local_buildcache(args)
